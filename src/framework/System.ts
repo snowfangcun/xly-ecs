5c17a3252f84f41b2cfd20539bfe7f2b0479c696
方寸 <1821075638@qq.com>
2025-07-24 17:58:04 +0800

    feat(framework): 添加系统类和世界中的系统管理功能
    - 新增 System 类，定义系统的基本属性和方法
    - 在 World 类中添加系统管理功能，包括添加系统和移除系统
    - 实现系统的优先级排序和生命周期方法调用

--- src/framework/System.ts
+++ src/framework/System.ts
@@ -1,1 +1,52 @@
+import type { World } from './world'
 
+/**
+ * 所有系统的基类
+ */
+export abstract class System {
+  private _enabled = true
+  private _priority = 0
+  protected world: World | undefined = undefined
+  /**
+   * Get system enabled state
+   */
+  get enabled(): boolean {
+    return this._enabled
+  }
+
+  /**
+   * 设置系统启用状态
+   */
+  set enabled(value: boolean) {
+    this._enabled = value
+  }
+
+  /**
+   * 获取系统优先级（较高值先执行）
+   */
+  get priority(): number {
+    return this._priority
+  }
+
+  /**
+   * 设置系统优先级
+   */
+  set priority(value: number) {
+    this._priority = value
+  }
+
+  /**
+   * 系统添加到世界时调用
+   * @param world 系统被添加到的世界实例
+   */
+  onAddedToWorld(world: World): void {
+    this.world = world
+  }
+
+  /**
+   * 系统从世界移除时调用
+   */
+  onRemovedFromWorld(): void {
+    this.world = undefined
+  }
+}
--- src/framework/World.ts
+++ src/framework/World.ts
@@ -1,6 +1,8 @@
 import { Entity, type EntityId } from './entity'
+import type { System } from './system'
 export class World {
   private _entities: Map<EntityId, Entity> = new Map()
+  private readonly _systems: System[] = []
 
   /**
    * 创建实体
@@ -26,5 +28,32 @@ export class World {
    */
   removeEntityById(entityId: EntityId): void {
     this._entities.delete(entityId)
+  }
+
+  /**
+   * 向世界中添加系统
+   */
+  addSystem(system: System): this {
+    this._systems.push(system)
+    /* 按照系统的优先级进行排序 */
+    this._systems.sort((a, b) => b.priority - a.priority)
+    /* 调用系统的onAddedToWorld生命周期方法 */
+    system.onAddedToWorld(this)
+    return this
+  }
+
+  /**
+   * 从世界中移除系统
+   * @param system
+   * @returns
+   */
+  removeSystem(system: System): this {
+    const index = this._systems.indexOf(system)
+    if (index !== -1) {
+      this._systems.splice(index, 1)
+      /* 调用系统的onRemovedFromWorld生命周期方法 */
+      system.onRemovedFromWorld()
+    }
+    return this
   }
 }
